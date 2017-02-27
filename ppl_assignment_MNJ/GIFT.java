6442039f1f6eb5f0efc102dd42d654941ef37bcc
UltraLazyCat <vtrippymane@gmail.com>
2017-02-27 06:38:27 +0530

    Add files via upload

---
+++ ppl_assignment_MNJ/GIFT.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//package q2;
+
+/**
+ *
+ * @author HP
+ */
+public class GIFT {
+    String Type;
+    int Price;
+    int Value;
+    
+    public GIFT(String type,int price,int value)
+    {
+        Type = type;
+        Price = price;
+        Value = value;
+    }
+}
